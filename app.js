e1171f003e3e3f1e25f53913f299d44e2b1c4f64
AdrienCodesCode <adrien.dousse@gmail.com>
2025-10-06 17:16:53 +0700

    preparing for deployment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 import indexRouter from "./routes/index.js"
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
